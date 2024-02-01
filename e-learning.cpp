a67b1804198c46b35937a2f3820cac05a073a3f6
aliha ashfaq <aliha ashfaq@gmail.com>
2024-02-01 12:19:41 +0500

    add branch

---
+++ e-learning.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+class e-learning
+{
+      private:
+              int id;
+              string name;
+      public:
+      void setId(int id)
+      { id=id;
+           }
+      void setName(string name)
+      {
+           name=name;
+           }
+      int getId()
+      {
+          return id;
+          }
+      string getName()
+      {
+             return name;
+             }
+      void setName();
+
+      int getId();
+
+      int getId()
+      {
+          return id;
+          }
+      string getName();
+      };
+      int main()
+      {
+          }
